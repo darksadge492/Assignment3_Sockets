48dcf523065382e7e7ba2050dba795e23b2065ea
Brendan <brendanom99@gmail.com>
2023-03-12 21:54:47 -0400

    Almost done competitive, just need to update text.

--- app.js
+++ app.js
@@ -68,6 +68,57 @@ io.on('connection', (socket) => {
         console.log('win check event triggered(app.js)');
         io.emit('update_win');
     });
+
+    socket.on('reset', () => {
+        console.log('reset tic-tac-toe event triggered(app.js)');
+        io.emit('reset_tictactoe');
+    });
+
+    socket.on('pressSquareOne', () => {
+        console.log('pressed square one');
+        io.emit('press_ONE');
+    });
+
+    socket.on('pressSquareTwo', () => {
+        console.log('pressed square two');
+        io.emit('press_TWO');
+    });
+
+    socket.on('pressSquareThree', () => {
+        console.log('pressed square three');
+        io.emit('press_THREE');
+    });
+
+    socket.on('pressSquareFour', () => {
+        console.log('pressed square four');
+        io.emit('press_FOUR');
+    });
+
+    socket.on('pressSquareFive', () => {
+        console.log('pressed square five');
+        io.emit('press_FIVE');
+    });
+
+    socket.on('pressSquareSix', () => {
+        console.log('pressed square six');
+        io.emit('press_SIX');
+    });
+
+    socket.on('pressSquareSeven', () => {
+        console.log('pressed square seven');
+        io.emit('press_SEVEN');
+    });
+
+    socket.on('pressSquareEight', () => {
+        console.log('pressed square eight');
+        io.emit('press_EIGHT');
+    });
+
+    socket.on('pressSquareNine', () => {
+        console.log('pressed square nine');
+        io.emit('press_NINE');
+    });
+
 });
 
 app.use(express.static(__dirname + '/public'));
